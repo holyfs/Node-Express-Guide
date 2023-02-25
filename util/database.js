8e70a98238afeb517815ddeb73e482f2549ad7c0
Ely Cruz <elygabriel.cruz@avantgardeit.es>
2023-02-25 22:20:01 +0100

    add sequelize

--- util/database.js
+++ util/database.js
@@ -1,10 +1,3 @@
-const mysql = require('mysql2');
+const Sequelize = require('sequelize');
 
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'node-complete',
-    password: '123456'
-});
-
-module.exports = pool.promise();
+exports.sequelize = new Sequelize('node-complete','root','123456',{dialect: 'mysql', host:'localhost'});
