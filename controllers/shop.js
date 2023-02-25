761ad840ea10006e71226124522256f8c1a49cda
Ely Cruz <elygabriel.cruz@avantgardeit.es>
2023-02-25 22:01:21 +0100

    Add findByID

--- controllers/shop.js
+++ controllers/shop.js
@@ -15,14 +15,17 @@ exports.getProducts = (req, res, next)=>{
 
 exports.getProduct = (req, res, next)=>{
     const productId = req.params.productId;
-    Product.findById(productId, product => {
-        res.render('shop/product-detail',{
+    Product.findById(productId)
+    .then(([product])=>{
+            res.render('shop/product-detail',{
             product: product,
             pageTitle: product.title,
             path:'/products'
-        });
-    });
-};
+        })
+    })
+    .catch(err=>console.error(err))
+    }
+
 
 exports.getIndex = (req , res, next)=>{
     Product.fetchAll()
