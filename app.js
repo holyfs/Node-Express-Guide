c251221c01a7e6fd9dcd2b5a70cb9d6d4af69ccc
Ely Cruz <elygabriel.cruz@avantgardeit.es>
2023-02-26 14:36:32 +0100

    fix middleware for user in app.js

--- app.js
+++ app.js
@@ -21,6 +21,15 @@ const notFound = require('./routes/not-found.js');
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use((req, res, next)=>{
+    User.findByPk(1)
+    .then(user=>{
+        req.user=user;
+        next();
+    })
+    .catch(err=>{console.error(err)})
+})
+
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 app.use(notFound);
@@ -32,7 +41,7 @@ sequelize
     .sync({ force: true })
     .then(result => {
         return User.findByPk(1);
-        //        console.log(result);
+//        console.log(result);
     })
     .then(user => {
         if (!user) {
@@ -41,7 +50,7 @@ sequelize
         return user;
     })
     .then(user=>{
-        console.log(user);
+//        console.log(user);
         app.listen(3000);
     })
     .catch(err => {
