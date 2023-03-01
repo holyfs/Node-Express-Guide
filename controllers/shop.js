6e70082b91c42c10b761cf0a4db6c8dc565872f7
Ely Cruz <elygabriel.cruz@avantgardeit.es>
2023-03-01 22:43:42 +0100

    fix cart and cartItem

--- controllers/shop.js
+++ controllers/shop.js
@@ -60,18 +60,52 @@ exports.getCart = (req, res, next)=>{
 
 exports.postCart = (req, res, next)=>{
     const productId = req.body.productId;
-    Product.findById(productId, (product)=>{
-        Cart.addProduct(productId, product.price);
-    });
-    res.redirect('/cart');
+    let fetchCart;
+    let newQuantity = 1;
+    req.user
+        .getCart()
+        .then(cart=>{
+            fetchCart = cart;
+            return cart.getProducts({where: {id: productId}})
+        })
+        .then(products=>{
+            let product;
+            if(products.length > 0){
+                product = products[0]
+            } 
+            if(product){
+               const oldQuantity = product.cartItem.quantity;
+               newQuantity = oldQuantity + 1;
+               return product;
+            }
+            return Product.findByPk(productId)
+        })
+        .then(product=>{
+            return fetchCart.addProduct(product,{
+                through: {quantity: newQuantity}
+               })
+        })
+        .then(()=>{
+            res.redirect('/cart')
+        })
+        .catch(err=>{console.error(err)})
 };
 
 exports.postCartDeleteProduct = (req, res, next)=>{
     const productId= req.body.productId;
-    Product.findById(productId, product=>{
-        Cart.deleteProduct(productId, product.price);
-        res.redirect('/');
-    });
+    req.user
+        .getCart()
+        .then(cart=>{
+            return cart.getProducts({where: {id: productId}})
+        })
+        .then(products=>{
+            const product = products[0];
+            return product.cartItem.destroy();
+        })
+        .then(result=>{
+            res.redirect('/cart');
+        })
+        .catch(err=>console.error(err))
 }
 
 exports.getOrders = (req, res, next)=>{
