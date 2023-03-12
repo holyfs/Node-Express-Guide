6845fb70ee082f2cf99fb1bdf93cce05c979ee49
Ely Cruz <elygabriel.cruz@avantgardeit.es>
2023-03-12 19:23:26 +0100

    Connect to mongodb

--- app.js
+++ app.js
@@ -2,14 +2,8 @@ const express = require('express');
 const path = require('path');
 
 const bodyParser = require('body-parser');
+const mongoConnect = require('./util/database');
 
-const sequelize = require('./util/database.js');
-const Product = require('./models/product.js');
-const User = require('./models/user.js');
-const Cart = require('./models/cart.js');
-const CartItem = require('./models/cart-item.js');
-const Order = require('./models/order.js');
-const OrderItem = require('./models/order-item.js');
 
 
 const app = express();
@@ -17,56 +11,28 @@ const app = express();
 app.set('view engine', 'pug');
 app.set('views', 'views');
 
-const adminRoutes = require('./routes/admin.js');
+/* const adminRoutes = require('./routes/admin.js');
 const shopRoutes = require('./routes/shop.js');
-const notFound = require('./routes/not-found.js');
+const notFound = require('./routes/not-found.js'); */
 
 
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next)=>{
-    User.findByPk(1)
+/*     User.findByPk(1)
     .then(user=>{
         req.user=user;
         next();
     })
-    .catch(err=>{console.error(err)})
+    .catch(err=>{console.error(err)}) */
 })
 
-app.use('/admin', adminRoutes);
+/* app.use('/admin', adminRoutes);
 app.use(shopRoutes);
-app.use(notFound);
+app.use(notFound); */
 
-Product.belongsTo(User, { constraits: true, onDelete: 'CASCADE' });
-User.hasMany(Product);
-User.hasOne(Cart);
-Cart.belongsToMany(Product, {through: CartItem});
-Product.belongsToMany(Cart, {through: CartItem});
-Order.belongsTo(User);
-User.hasMany(Order);
-Order.belongsToMany(Product, {through: OrderItem});
-
-sequelize
-//    .sync({ force: true })
-    .sync()
-    .then(result => {
-        return User.findByPk(1);
-//        console.log(result);
-    })
-    .then(user => {
-        if (!user) {
-            User.create({ name: 'Ely', email: 'test@test.es' })
-        }
-        return user;
-    })
-    .then(user=>{
-//        console.log(user);
-        return user.createCart();
-    })
-    .then(cart=>{
-        app.listen(3000);
-    })
-    .catch(err => {
-        console.error(err);
-    })
+mongoConnect((client)=>{
+    console.log(client);
+    app.listen(3000);
+});
--- util/database.js
+++ util/database.js
@@ -1,5 +1,15 @@
-const Sequelize = require('sequelize');
+const mongodb= require('mongodb');
+const mongoClient = mongodb.MongoClient;
 
-const sequelize = new Sequelize('node-complete','root','123456',{dialect: 'mysql', host:'localhost'});
+const mongoConnect = (callback) => {
+    mongoClient.connect('mongodb+srv://elycruzdev:o6AHCTZ5LnRsyj0b@cluster0.detozxi.mongodb.net/?retryWrites=true&w=majority')
+        .then(client => {
+            console.log('Connected!');
+            callback(client);
+        })
+        .catch(err => {
+            console.error(err);
+        });
+}
 
-module.exports = sequelize;
+module.exports = mongoConnect;
