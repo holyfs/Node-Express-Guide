ca0348b75b2960724c9c416cb025f0fcbbfac42f
Ely Cruz <elygabriel.cruz@avantgardeit.es>
2023-03-14 10:53:37 +0100

    Add update product,Add save product,Fix minor things

--- models/product.js
+++ models/product.js
@@ -1,18 +1,27 @@
+const mongodb = require('mongodb');
 const getDb = require('../util/database').getDb;
 
 class Product {
-  constructor(title, price, description, imageUrl) {
+  constructor(title, price, description, imageUrl, id) {
     this.title = title;
     this.price = price;
     this.description = description;
     this.imageUrl = imageUrl;
+    this._id= new mongodb.ObjectId(id);
   }
 
   save() {
     const db = getDb();
-    return db
-      .collection('products')
+    let dbOp;
+    if(this._id){
+      dbOp=db
+        .collection('products')
+        .updateOne({ _id: this._id },{ $set: this });
+    }else{
+      dbOp= db.collection('products')
       .insertOne(this)
+    }
+    return dbOp
       .then(result => {
         console.log(result);
       })
@@ -34,6 +43,21 @@ class Product {
       .catch(err => {
         console.log(err);
       });
+  }
+
+  static findById(id) {
+    const db = getDb();
+    return db
+      .collection('products')
+      .find({ _id: new mongodb.ObjectId(id) })
+      .next()
+      .then(product=>{
+        console.log(product);
+        return product;
+      })
+      .catch(err=>{
+        console.error(err);
+      })
   }
 }
 
