ba50bca5156627b89381adb07ac66ae2582a48a4
Ely Cruz <elygabriel.cruz@avantgardeit.es>
2023-03-15 12:11:40 +0100

    Store product in user cart

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use((req, res, next)=>{
     User.findById("6410681590c18b1b9751e0ee")
     .then(user=>{
-        req.user=user;
+        req.user=new User(user.name, user.email, user.cart, user._id);
         next();
     })
     .catch(err=>{console.error(err)})
--- controllers/admin.js
+++ controllers/admin.js
@@ -13,7 +13,7 @@ exports.postAddProduct = (req, res, next) => {
   const title = req.body.title;
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
-  const description = req.body.description;
+  const description = req.body.description.trim();
   const product = new Product(title, price, description, imageUrl, null, req.user._id);
   product
     .save()
