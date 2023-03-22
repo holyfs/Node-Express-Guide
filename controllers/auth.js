37e98dc60aedb6ec83db13a63a6792bd18146139
Ely Cruz <elygabriel.cruz@avantgardeit.es>
2023-03-22 17:39:56 +0100

    Fix controllers

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,9 +1,6 @@
+const User = require('../models/user');
+
 exports.getLogin = (req, res, next) => {
-  //   const isLoggedIn = req
-  //     .get('Cookie')
-  //     .split(';')[1]
-  //     .trim()
-  //     .split('=')[1] === 'true';
   console.log(req.session.isLoggedIn);
   res.render('auth/login', {
     path: '/login',
@@ -13,6 +10,11 @@ exports.getLogin = (req, res, next) => {
 };
 
 exports.postLogin = (req, res, next) => {
-  req.session.isLoggedIn = true;
-  res.redirect('/');
+  User.findById('6419ff9b1b45f875a33f31ba')
+  .then(user => {
+    req.session.isLoggedIn = true;
+    req.session.user = user;
+    res.redirect('/');
+  })
+  .catch(err=>{console.error(err)})
 };
