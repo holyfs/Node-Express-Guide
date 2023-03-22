4ce2bbbc49e6b57bfd4b3f4950b45c61f6754380
Ely Cruz <elygabriel.cruz@avantgardeit.es>
2023-03-22 10:16:49 +0100

    Fix relations in cart, products, user

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(bodyParser.urlencoded({extended:false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-    User.findById('5bab316ce0a7c75f783cb8a8')
+    User.findById('6419ff9b1b45f875a33f31ba')
       .then(user => {
         req.user = user;
         next();
@@ -48,6 +48,7 @@ mongoose
             }
           });
         app.listen(3000);
+        console.log("Conected!")
     }).catch(err=>{
         console.error(err);
     })  
