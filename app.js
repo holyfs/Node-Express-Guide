484271e5f395aa90c44525db7fe2a09f5392377e
Ely Cruz <elygabriel.cruz@avantgardeit.es>
2023-03-22 14:03:10 +0100

    Store sesion in db

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
 const path = require('path');
+require('dotenv').config();
 
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
+
+const userCred=process.env.DB_USERNAME;
+const userPass=process.env.DB_PASSWORD
+
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
 
+const MONGODB_URI =
+  `mongodb+srv://${userCred}:${userPass}@cluster0.detozxi.mongodb.net/shop?readPreference=primary`;
+
 const app = express();
+const store = new MongoDBStore({
+  uri: MONGODB_URI,
+  collection: 'sessions'
+});
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -20,11 +33,16 @@ const authRoutes = require('./routes/auth');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(
-  session({ secret: 'my secret', resave: false, saveUninitialized: false })
+  session({
+    secret: 'my secret',
+    resave: false,
+    saveUninitialized: false,
+    store: store
+  })
 );
 
 app.use((req, res, next) => {
-  User.findById('6419ff9b1b45f875a33f31ba')
+  User.findById('5bab316ce0a7c75f783cb8a8')
     .then(user => {
       req.user = user;
       next();
@@ -39,9 +57,7 @@ app.use(authRoutes);
 app.use(errorController.get404);
 
 mongoose
-  .connect(
-    'mongodb+srv://elycruzdev:o6AHCTZ5LnRsyj0b@cluster0.detozxi.mongodb.net/shop?readPreference=primary'
-  )
+  .connect(MONGODB_URI)
   .then(result => {
     User.findOne().then(user => {
       if (!user) {
